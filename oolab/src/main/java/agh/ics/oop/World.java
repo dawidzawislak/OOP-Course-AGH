749ae7190dd34299c7fbe8cc0465920bc397ef65
dawidzawislak <zawislak.dawid02@gmail.com>
2023-10-23 12:17:56 +0200

    Initialized project, added World, MoveDirection and optionParser

---
+++ oolab/src/main/java/agh/ics/oop/World.java
@@ -0,0 +1,26 @@
+package agh.ics.oop;
+
+import agh.ics.oop.model.MoveDirection;
+
+public class World {
+    public static void main(String[] args) {
+        System.out.println("system wystartował");
+
+        run(args);
+
+        System.out.println("system zakończył działanie");
+    }
+
+    private static void run(String[] directions) {
+        MoveDirection[] dirs = OptionsParser.convertToDir(directions);
+
+        for (MoveDirection dir : dirs) {
+            switch (dir) {
+                case FORWARD -> System.out.println("zwierzak idzie do przodu");
+                case BACKWARD -> System.out.println("zwierzak idzie do tyłu");
+                case LEFT -> System.out.println("zwierzak idzie w lewo");
+                case RIGHT -> System.out.println("zwierzak idzie w prawo");
+            }
+        }
+    }
+}
